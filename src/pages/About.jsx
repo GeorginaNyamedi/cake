ca0f26a67edbb53f0259a9e88d99aea9faf910e5
GeorginaNyamedi <110406742+GeorginaNyamedi@users.noreply.github.com>
2022-10-17 06:46:34 +0000

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,54 @@
+
+import React  from 'react';
+
+
+function About() {
+
+
+  
+ 
+  return (
+    <div>
+      <h2> Get the Best Cakes Here!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio
+        animi quia est repellat aut necessitatibus deserunt nobis culpa,
+        quibusdam doloribus. Dolorem asperiores explicabo sapiente nostrum quam
+        neque, fugiat cumque? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Cupiditate odio animi quia est repellat aut necessitatibus
+        deserunt nobis culpa, quibusdam doloribus. Dolorem asperiores explicabo
+        sapiente nostrum quam neque, fugiat cumque? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Cupiditate odio animi quia est repellat
+        aut necessitatibus deserunt nobis culpa, quibusdam doloribus. Dolorem
+        asperiores explicabo sapiente nostrum quam neque, fugiat cumque? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio animi
+        quia est repellat aut necessitatibus deserunt nobis culpa, quibusdam
+        doloribus. Dolorem asperiores explicabo sapiente nostrum quam neque,
+        fugiat cumque? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Cupiditate odio animi quia est repellat aut necessitatibus deserunt
+        nobis culpa, quibusdam doloribus. Dolorem asperiores explicabo sapiente
+        nostrum quam neque, fugiat cumque? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Cupiditate odio animi quia est repellat
+        aut necessitatibus deserunt nobis culpa, quibusdam doloribus. Dolorem
+        asperiores explicabo sapiente nostrum quam neque, fugiat cumque? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio animi
+        quia est repellat aut necessitatibus deserunt nobis culpa, quibusdam
+        doloribus. Dolorem asperiores explicabo sapiente nostrum quam neque,
+        fugiat cumque? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Cupiditate odio animi quia est repellat aut necessitatibus deserunt
+        nobis culpa, quibusdam doloribus. Dolorem asperiores explicabo sapiente
+        nostrum quam neque, fugiat cumque? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Cupiditate odio animi quia est repellat
+        aut necessitatibus deserunt nobis culpa, quibusdam doloribus. Dolorem
+        asperiores explicabo sapiente nostrum quam neque, fugiat cumque? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Cupiditate odio animi
+        quia est repellat aut necessitatibus deserunt nobis culpa, quibusdam
+        doloribus. Dolorem asperiores explicabo sapiente nostrum quam neque,
+        fugiat cumque?
+      </p>
+    </div>
+  );
+}
+
+export default About
+
